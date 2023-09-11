9ede46fc4e75cb33ac98afacf2bc65fd70f707ed
MaxiFerrez <maxiferrez@gmail.com>
2023-09-11 00:15:40 -0300

    se agregaron rutas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Personajes from './components/personajes/Personajes';
+import Login from './components/login/Login';
 
-function App() {
+function RoutesApp() {
   return (
-    <div className="App">
-      <Personajes />
-    </div>
+    <>
+      <div >NAVBAR</div>
+
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+        <Route path="/personajes" element={<Personajes />} />
+
+      </Routes>
+      <div >Footer</div>
+
+    </>
+
   );
 }
+  function Routerwrapper() {
+    return (
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
+    )
+}
 
-export default App;
+export default Routerwrapper;
