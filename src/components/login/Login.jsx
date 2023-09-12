3752b0eac5a9132aa83a1df87d6955d546c4e5be
Enrique Espinosa <quiqueenri2016@gmail.com>
2023-09-12 10:56:58 -0300

    otro commit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // Utiliza useNavigate para la navegación
 
   const handlePasswordChange = (event) => {
     const newPassword = event.target.value;
     setPassword(newPassword);
     console.clear(); // Borra la consola antes de imprimir la nueva contraseña
     console.log(newPassword);
+  };
+
+  const handleLoginClick = () => {
+    // Redirige a la ruta deseada cuando se hace clic en el botón
+    navigate('/personajes'); // Cambia '/personajes' a la ruta que desees
   };
 
   return (
@@ -29,7 +36,9 @@ function Login() {
             onChange={handlePasswordChange}
           />
         </div>
-        <button  className="login-button">Iniciar Sesión</button>
+        <button className="login-button" onClick={handleLoginClick}>
+          Iniciar Sesión
+        </button>
         <div className="forgot-password">
           <a href="#">Olvidé mi contraseña</a>
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Navbar.css';
+import simpson from '../../img/gif2.gif';
 
 function Navbar() {
   return (
     <nav className="navbar">
+       <div className="logo-nav">
+        <img src={simpson} alt="Mi Logo" />
+      </div>
       <ul className="navbar-list">
         <li className="navbar-item">
           <a href="/inicia" className="navbar-link">Api1</a>
         </li>
         <li className="navbar-item">
           <a href="/hola" className="navbar-link">Api2</a>
+        </li>
+        <li className="navbar-item">
+          <a href="/" className="navbar-link">salir</a>
         </li>
       </ul>
     </nav>
